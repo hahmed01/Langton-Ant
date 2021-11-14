a6a774b8aede7a68301c77259764475b8f224585
hahmed01 <40370629+hahmed01@users.noreply.github.com>
2021-11-14 11:31:53 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 /*********************************************************************
 ** Program name: Langton's Ant
 ** Author: Haya Ahmed
-** Date: October 10, 2018 (used 3 bonus days on this assignment)
-** Description: Main of Langton's Ant. The simulation is in menu().
 *********************************************************************/
 
 #include "Ant.hpp"
